5e81b70637c67b389ce0336b9aa5fd079d99ba14
Camron <rennie.camron@gmail.com>
2019-06-28 19:51:38 -0700

    Added login landing page

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const flash = require('express-flash');
+const session = require('express-session');
+const uuidv1 = require('uuid/v1');
+const { Pool } = require('pg');
+
+const connectionString = ''; // Connection string needs to go here!
+
+const pool = new Pool({
+    connectionString: connectionString,
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/login'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
