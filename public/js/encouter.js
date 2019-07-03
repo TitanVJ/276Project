45a6bb939f9488660ad5713abab8591772c2fc21
VJ <vijenderbakhshi@gmail.com>
2019-07-02 21:39:23 -0700

    undid encouter file deletion.

---
+++ public/js/encouter.js
@@ -0,0 +1,37 @@
+var keyStatus = $('#key')[0];
+var count = $('#counter')[0];
+var moves = $('#moves')[0];
+
+var socket = io('localhost:8080');
+var encounter = false;
+
+socket.on('hi', (msg)=>{
+    alert(msg);
+})
+
+socket.on('no', ()=>{
+    console.log('no encounter');
+});
+
+socket.on('encounter',(profObj)=>{
+    encounter = true;
+    count.innerHTML++;
+    // this will make a coffee cup?(something icon) appear on teh map that user will click to engage capture mode
+    var img = $('#tempEncounter')[0];
+    img.src = '../images/crocker.png';
+
+    var details = $('#profDetails')[0];
+    details.innerHTML = JSON.stringify(profObj);
+
+    // this will later be adapted to delete the spawned icon if the user doesn't click on it in time
+    setTimeout(()=>{encounter=false;img.src = '';details.innerHTML = ''; console.log('removed img');}, 30000);
+});
+
+$(document).keydown((e)=>{
+    // check for wasd only keys 
+    if(['w', 'a', 's', 'd'].includes(e.key)){
+        keyStatus.innerHTML = e.key.toUpperCase();
+        moves.innerHTML++;
+        socket.emit('move', encounter);
+    }
+});
--- public/js/main.js
+++ public/js/main.js
@@ -76,26 +76,19 @@ function update() {
     if (cursors.left.isDown)
     {
         player.body.velocity.x = -300;
-        move('left');
     }
     else if (cursors.right.isDown)
     {
-        player.body.velocity.x = 300;
-        move('right');
-            
+		player.body.velocity.x = 300;    
 	}
 
     if (cursors.up.isDown)
     {
         player.body.velocity.y = -300;
-        move('up');
-
     }
     else if (cursors.down.isDown)
     {
-        player.body.velocity.y = 300;
-        move('down');
-        
+    	player.body.velocity.y = 300;
     }
 
     
@@ -105,41 +98,4 @@ function update() {
 
 function render() {
 
-}
-
-//VJ's encounter code
-
-var keyStatus = $('#key')[0];
-var count = $('#counter')[0];
-var moves = $('#moves')[0];
-
-var socket = io();
-var encounter = false;
-
-socket.on('hi', (msg)=>{
-    alert(msg);
-})
-
-socket.on('no', ()=>{
-    console.log('no encounter');
-});
-
-socket.on('encounter',(profObj)=>{
-    encounter = true;
-    count.innerHTML++;
-    // this will make a coffee cup?(something icon) appear on teh map that user will click to engage capture mode
-    var img = $('#tempEncounter')[0];
-    img.src = '../images/crocker.png';
-
-    var details = $('#profDetails')[0];
-    details.innerHTML = JSON.stringify(profObj);
-
-    // this will later be adapted to delete the spawned icon if the user doesn't click on it in time
-    setTimeout(()=>{encounter=false;img.src = '';details.innerHTML = ''; console.log('removed img');}, 30000);
-});
-
-function move(key){
-        keyStatus.innerHTML = key.toUpperCase();
-        moves.innerHTML++;
-        socket.emit('move', encounter);
 }
