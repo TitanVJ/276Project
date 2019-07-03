a77eee588e666271e2375945311123d3e0559e5a
VJ <vijenderbakhshi@gmail.com>
2019-07-02 21:20:42 -0700

    moved encouter.js code to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -76,19 +76,26 @@ function update() {
     if (cursors.left.isDown)
     {
         player.body.velocity.x = -300;
+        move();
     }
     else if (cursors.right.isDown)
     {
-		player.body.velocity.x = 300;    
+        player.body.velocity.x = 300;
+        move();
+            
 	}
 
     if (cursors.up.isDown)
     {
         player.body.velocity.y = -300;
+        move();
+
     }
     else if (cursors.down.isDown)
     {
-    	player.body.velocity.y = 300;
+        player.body.velocity.y = 300;
+        move();
+        
     }
 
     
@@ -100,3 +107,39 @@ function render() {
 
 }
 
+//VJ's encounter code
+
+var keyStatus = $('#key')[0];
+var count = $('#counter')[0];
+var moves = $('#moves')[0];
+
+var socket = io();
+var encounter = false;
+
+socket.on('hi', (msg)=>{
+    alert(msg);
+})
+
+socket.on('no', ()=>{
+    console.log('no encounter');
+});
+
+socket.on('encounter',(profObj)=>{
+    encounter = true;
+    count.innerHTML++;
+    // this will make a coffee cup?(something icon) appear on teh map that user will click to engage capture mode
+    var img = $('#tempEncounter')[0];
+    img.src = '../images/crocker.png';
+
+    var details = $('#profDetails')[0];
+    details.innerHTML = JSON.stringify(profObj);
+
+    // this will later be adapted to delete the spawned icon if the user doesn't click on it in time
+    setTimeout(()=>{encounter=false;img.src = '';details.innerHTML = ''; console.log('removed img');}, 30000);
+});
+
+function move(){
+        keyStatus.innerHTML = e.key.toUpperCase();
+        moves.innerHTML++;
+        socket.emit('move', encounter);
+}
