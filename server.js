9814536202c1df021894acf999cb73295d23c76a
nikolaf <nikolaf@sfu.ca>
2019-07-28 02:29:39 -0700

    updating position in database on set intervals 22

--- server.js
+++ server.js
@@ -541,13 +541,15 @@ app.get('/updateLocation',async(req,res)=>{
 			if (response){
 				console.log(response.rows.length,req.session.user_name);
 				if(response.rows.length > 0) {
-					pool.query("UPDATE userPos SET X_pos=$1,Y_pos=$2 WHERE user_name='$3'",[parseInt(req.query.x),parseInt(req.query.y),req.session.user_name], (err, response) => {
+					Let values = [parseInt(req.query.x),parseInt(req.query.y),req.session.user_name];
+					pool.query("UPDATE userPos SET X_pos=$1,Y_pos=$2 WHERE user_name='$3'", values, (err, response) => {
 						if(err) {
 							console.log(err);
 						}
 					});
 				} else {
-					pool.query("INSERT INTO userPos(user_name,X_pos,Y_pos) VALUES ('$1',$2,$3)",[req.session.user_name,req.query.x,req.query.y], (err, response) => {
+					Let values = [req.session.user_name,parseInt(req.query.x),parseInt(req.query.y)];
+					pool.query("INSERT INTO userPos(user_name,X_pos,Y_pos) VALUES ('$1',$2,$3)", values, (err, response) => {
 						if(err) {
 							console.log(err);
 						}
