b7a499ccfeef068c0ed7500662b7d1f728481d80
nikolaf <nikolaf@sfu.ca>
2019-07-28 12:19:17 -0700

    updating position in database on set intervals 27

--- server.js
+++ server.js
@@ -538,9 +538,9 @@ app.get('/updateLocation',async(req,res)=>{
 			if(err) {
 				console.log(err);
 			}
-			/*if (response){
+			if (response){
 				if(response.rows.length > 0) {
-					let values = [parseInt(req.query.x),parseInt(req.query.y),req.session.user_name];
+					let values = [parseInt(req.query.x),parseInt(req.query.y),String(req.session.user_name)];
 					pool.query("UPDATE userPos SET X_pos=$1,Y_pos=$2 WHERE user_name='$3'", values, (err, response) => {
 						if(err) {
 							console.log(err);
@@ -548,7 +548,7 @@ app.get('/updateLocation',async(req,res)=>{
 					});
 				} else {
 					console.log(req.session.user_name,req.query.x,req.query.y);
-					let values = [req.session.user_name,parseInt(req.query.x),parseInt(req.query.y)];
+					let values = [String(req.session.user_name),parseInt(req.query.x),parseInt(req.query.y)];
 					pool.query("INSERT INTO userPos(user_name,X_pos,Y_pos) VALUES ('$1',$2,$3)", values, (err, response) => {
 						if(err) {
 							console.log(err);
@@ -558,7 +558,7 @@ app.get('/updateLocation',async(req,res)=>{
 			}
 			else {
 				console.log("no response from query");
-			}*/
+			}
 			res.status(200);
 		});
 		res.end();
