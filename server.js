93f420b8e9b990ffe1aaff9e8c6c5d0cd9eabe5d
nikolaf <nikolaf@sfu.ca>
2019-07-28 02:11:37 -0700

    updating position in database on set intervals 17

--- server.js
+++ server.js
@@ -533,7 +533,7 @@ app.get('/changeUserStatus', function(req, res) {
     });
 });
 app.get('/updateLocation',async(req,res)=>{
-	if (!req.session.user_name){
+	if (req.session.user_name){
 		pool.query("SELECT user_name FROM userPos WHERE user_name=$1",[req.query.user], (err, response) => {
 			if(err) {
 				console.log(err);
