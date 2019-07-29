2a4ad051285f4bb233f0a64c0e9d29c8551c8818
dkovacev <dkovacev@sfu.ca>
2019-07-29 13:46:52 -0700

    Modal set up

--- public/js/game.js
+++ public/js/game.js
@@ -2,9 +2,6 @@
 import main from './main.js';
 import catchState from '../states/catchState.js';
 
-
-document.getElementById("userInv").addEventListener("click", userInv);
-
 class Game extends Phaser.Game {
     constructor(){
         super($(window).width(), ($(window).height()-145), Phaser.CANVAS, 'phaser');
@@ -16,25 +13,5 @@ class Game extends Phaser.Game {
         this.state.start('main');
     }
 }
-
+//
 new Game();
-
-// function userInv(e) {
-//
-//
-//     $.ajax({
-//         method: 'GET',
-//         url: '/toInventoryGame',
-//         success: function(data) {
-//             $('#item_body').empty();
-//             $.each(data.results, function() {
-//                 $('<tr>').attr('id', `${this.iphoto_id}`).appendTo('#item_body');
-//                 $('<td>').html(`${this.item_name}`).appendTo('#'+this.iphoto_id);
-//                 $('<td>').html(`${this.quantity}`).appendTo('#'+this.iphoto_id);
-//
-//             });
-//
-//         }
-//     });
-//
-// };
