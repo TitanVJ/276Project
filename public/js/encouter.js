a2e0a35d9ee31578feac9f8a562d9218fb8bf659
dkovacev <dkovacev@sfu.ca>
2019-07-29 18:59:13 -0700

    Fixed Inventory pop up, and office hour uses properly run out.

--- public/js/encouter.js
+++ public/js/encouter.js
@@ -3,53 +3,6 @@ var encounter = false;
 var spawnball = false;
 var profInfo;
 
-
-var useTime = 0;
-var sessionUserName;
-
-// Control modal the appears when a user is clicked in the user list on the admin page
-$('#user_invent').on('show.bs.modal', function (e) {
-    $('#modal_title').html("Inventory for " + e.relatedTarget.id)
-    $('#use_item').attr('data-user-id', e.relatedTarget.id);
-
-    $.ajax({
-        method: 'GET',
-        url: `/toInventory?user=${e.relatedTarget.id}`,
-        success: function(data) {
-            $('#item_body').empty();
-            $.each(data.results, function() {
-                $('<tr>').attr('id', `${this.iphoto_id}`).appendTo('#item_body');
-                $('<td>').html(`${this.item_name}`).appendTo('#'+this.iphoto_id);
-                $('<td>').html(`${this.quantity}`).appendTo('#'+this.iphoto_id);
-
-            });
-
-        }
-    });
-});
-
-$('#use_item').confirmation({
-    rootSelector: '[data-toggle=confirmation]',
-    onConfirm: function () {
-        // Just changes the user status but does not drop the tables that only applied to them when they were a user.
-        if(Date.now()-useTime >= 60000){
-          $.ajax({
-              method: 'GET',
-              url: `/popAPill`,
-              success(data) {
-                useTime = Date.now();
-              }
-          });
-          console.log("Confirm");
-        }
-        else{
-          alert("You have recently used an Office Hour, please wait");
-        }
-    }
-});
-
-
-
 socket.on('hi', (msg)=>{
     alert(msg);
 })
@@ -63,6 +16,7 @@ socket.on('encounter',(profObj)=>{
     spawnball = true;
     encounter = true;
 });
+
 // document.getElementById("add").addEventListener("click", function(a){
 //   $.ajax({
 //       method:'get',
@@ -95,10 +49,15 @@ socket.on('encounter',(profObj)=>{
 $(document).keydown((e)=>{
     // check for wasd only keys
     if(['w', 'a', 's', 'd'].includes(e.key)){
-      if(Date.now()-useTime){
-        socket.emit('changeEncounter');
-      }
-
-      socket.emit('move', encounter);
+      $.ajax({
+          method:'get',
+          url:'/getUserName',
+          success: function(data){
+            console.log(data);
+            console.log("bruh");
+            encounterChance = data.encounter_chance
+            socket.emit('move', encounterChance);
+          }
+      });
     }
 });
--- public/js/game.js
+++ public/js/game.js
@@ -2,9 +2,52 @@
 import main from './main.js';
 import catchState from '../states/catchState.js';
 
+var useTime = 0;
+var sessionUserName;
+// Control modal the appears when a user is clicked in the user list on the admin page
+$('#user_invent').on('show.bs.modal', function (e) {
+    $('#modal_title').html("Inventory for " + e.relatedTarget.id)
+    $('#use_item').attr('data-user-id', e.relatedTarget.id);
+
+    $.ajax({
+        method: 'GET',
+        url: `/toInventory?user=${e.relatedTarget.id}`,
+        success: function(data) {
+            $('#item_body').empty();
+            $.each(data.results, function() {
+                $('<tr>').attr('id', `${this.iphoto_id}`).appendTo('#item_body');
+                $('<td>').html(`${this.item_name}`).appendTo('#'+this.iphoto_id);
+                $('<td>').html(`${this.quantity}`).appendTo('#'+this.iphoto_id);
+
+            });
+
+        }
+    });
+});
+
+$('#use_item').confirmation({
+    rootSelector: '[data-toggle=confirmation]',
+    onConfirm: function () {
+        // Just changes the user status but does not drop the tables that only applied to them when they were a user.
+        if(Date.now()-useTime >= 60000){
+          $.ajax({
+              method: 'GET',
+              url: `/popAPill`,
+              success(data) {
+                useTime = Date.now();
+              }
+          });
+          console.log("Confirm");
+        }
+        else{
+          alert("You have recently used an Office Hour, please wait");
+        }
+    }
+});
+
 class Game extends Phaser.Game {
     constructor(){
-        super($(window).width(), ($(window).height()-145), Phaser.CANVAS, 'phaser');
+        super(($(window).width()-200), ($(window).height()-145), Phaser.CANVAS, 'phaser');
 
         // add states
         this.state.add('main', main);
