7f93c0519eaa75187946fe06af3378f4b3a0a54d
nikolaf <nikolaf@sfu.ca>
2019-07-30 18:18:25 -0700

    test for position

--- test/postest.js
+++ test/postest.js
@@ -5,7 +5,7 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Give Position of Player', function() {
+describe('Create Account', function() {
    it('should add a user to the user table in the database', function(done) {
 
         chai.request(server).get('/get-num-users').end(function(err, res_1) {
@@ -53,15 +53,29 @@ describe('Login', function() {
         });
     })
 });
-describe('addCandy', function() {
-    it('Should add ProfHours', function(done) {
+describe('Position Update', function() {
+    it('Should Update the Location of Player', function(done) {
 
         chai.request(server)
-            .get('/addCandy')
+            .get('/updateLocation')
             .type('form')
             .send({
-                'username':'testing'
+                'user_name' : 'test',
+                'x_pos': '1000',
+                'y_pos': '1000'
+            })
+            .end(function(err, res) {
 
+                res.should.have.status(200);
+                done();
+        });
+        chai.request(server)
+            .get('/updateLocation')
+            .type('form')
+            .send({
+                'user_name' : 'test',
+                'x_pos': '6290',
+                'y_pos': '2620'
             })
             .end(function(err, res) {
 
@@ -70,15 +84,30 @@ describe('addCandy', function() {
         });
     })
 });
+describe('Position Get', function() {
+    it('Should Get the Old Location of Player', function(done) {
 
-describe('GrabItem', function() {
-    it('it should remove prof hours', function(done) {
-
+        
         chai.request(server)
-            .get('/popAPill')
+            .get('/getLocation')
             .type('form')
             .send({
-                'username':'testing'
+                'user_name' : 'test',
+                'x_pos': '1000',
+                'y_pos': '1000'
+            })
+            .end(function(err, res) {
+
+                res.should.have.status(200);
+                done();
+        });
+        chai.request(server)
+            .get('/getLocation')
+            .type('form')
+            .send({
+                'user_name' : 'test',
+                'x_pos': '3000',
+                'y_pos': '2500'
             })
             .end(function(err, res) {
 
